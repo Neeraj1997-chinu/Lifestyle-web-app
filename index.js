a1afe1201003421f75743f937e00020af310e3c6
Neeraj1997-chinu <58322212+Neeraj1997-chinu@users.noreply.github.com>
2021-01-01 21:21:46 +0530

    corousel added

--- index.js
+++ index.js
@@ -5,3 +5,23 @@ function close_nav() {
 function open_nav() {
   document.getElementById("side-menu").style.width = "250px";
 }
+
+var slideIndex = 0;
+slideshow();
+
+function slideshow() {
+  var i;
+  var slides = document.getElementsByClassName("corousel-image");
+  // console.log(slides);
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[slideIndex].style.display = "block";
+  console.log(slideIndex);
+  slideIndex++;
+  if (slideIndex > slides.length - 1) {
+    slideIndex = 0;
+  }
+  setTimeout(slideshow, 2000);
+}
